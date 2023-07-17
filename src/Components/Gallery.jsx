57b9da9ec6d876047714190987aa87009ad4edeb
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-17 15:06:43 -0500

    chore: fetch data using useQuery from API.

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,13 @@
+function Gallery() {
+  return (
+    <section className="gallery-container">
+      <h2>Amaze yourself</h2>
+      <h4>Our top picks for you</h4>
+      {/* Portraits section */}
+      <h2>More for you</h2>
+      <h4>Incredible pictures just for you.</h4>
+    </section>
+  )
+}
+
+export default Gallery
