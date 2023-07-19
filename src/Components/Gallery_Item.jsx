8561f7d50bbf1e44830026c7c1ae144135e8b937
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-18 21:57:10 -0500

    feat: null description message updated.

--- src/Components/Gallery_Item.jsx
+++ src/Components/Gallery_Item.jsx
@@ -9,10 +9,10 @@ function Gallery_Item(props) {
     <article>
       <img src={urls?.regular} alt={alt_description} />
       <h3 className="card-title">
-        {description?.slice(0, 10) ?? `Portrait pic of ${userPrompt}`}
+        {description?.slice(0, 10) ?? `Some ${userPrompt}`}
       </h3>
       <h5 className="card-subtitle">
-        {alt_description?.slice(0, 30) ??
+        {alt_description?.slice(0, 50) ??
           `Just a pretty pic for ${userPrompt}  `}
       </h5>
     </article>
