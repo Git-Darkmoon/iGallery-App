fe83b05832d76ae58442cad1d1a92a253d944136
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-19 16:21:50 -0500

    added: Content for the sidebar.

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,28 +1,20 @@
 import logo from "../assets/iGallery_favicon.svg"
 import { LiaTimesSolid } from "react-icons/lia"
 import { useGlobalContext } from "../context"
+import SidebarFooter from "./SidebarFooter"
 
 function Sidebar() {
   const { isSideOpen, closeSidebar } = useGlobalContext()
 
   return (
     <section className={`sidebar-overlay ${isSideOpen && "showSidebar"}`}>
-      <LiaTimesSolid onClick={closeSidebar} />
+      <LiaTimesSolid className="icon" onClick={closeSidebar} />
       <aside className="sidebar">
         <picture className="logo-container">
-          <img src={logo} alt="" id="logo" />
           <h1>iGallery App</h1>
+          <img src={logo} alt="" id="logo" />
         </picture>
-        <ul>
-          <li>
-            <a
-              href="https://unsplash.com/documentation#search-photos"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              API Documentation
-            </a>
-          </li>
+        <ul className="sidebar-links">
           <li>
             <a
               href="https://unsplash.com/"
@@ -32,7 +24,17 @@ function Sidebar() {
               Official Site
             </a>
           </li>
+          <li>
+            <a
+              href="https://unsplash.com/documentation#search-photos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              API Documentation
+            </a>
+          </li>
         </ul>
+        <SidebarFooter />
       </aside>
     </section>
   )
